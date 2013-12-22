f724611e46b62eb07c26df962429f818817d5dbf
Sven <enkelmagnus@gmail.com>
2013-12-22 20:19:23 +0100

    Several refactorizations. Customized JavaOsc and merged into project.

---
+++ ReaperToolbarRemote/src/com/illposed/osc/OSCPortOut.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2004-2006, C. Ramakrishnan / Illposed Software.
+ * All rights reserved.
+ *
+ * This code is licensed under the BSD 3-Clause license.
+ * See file LICENSE (or LICENSE.html) for more information.
+ */
+
+package com.illposed.osc;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * OSCPortOut is the class that sends OSC messages
+ * to a specific address and port.
+ *
+ * To send an OSC message, call send().
+ *
+ * An example based on
+ * {@link com.illposed.osc.OSCPortTest#testMessageWithArgs()}:
+ * <pre>
+	OSCPort sender = new OSCPort();
+	Object args[] = new Object[2];
+	args[0] = new Integer(3);
+	args[1] = "hello";
+	OSCMessage msg = new OSCMessage("/sayhello", args);
+	 try {
+		sender.send(msg);
+	 } catch (Exception e) {
+		 showError("Couldn't send");
+	 }
+ * </pre>
+ *
+ * @author Chandrasekhar Ramakrishnan
+ */
+public class OSCPortOut extends OSCPort implements Runnable {
+
+	
+
+	/**
+	 * Create an OSCPort that sends to address:port.
+	 * @param address the UDP address to send to
+	 * @param port the UDP port to send to
+	 */
+	public OSCPortOut(InetAddress address, int port)
+		throws SocketException
+	{
+		super(new DatagramSocket(), port);
+		this.address = address;
+	}
+	public void startSender(){
+		Thread t = new Thread(this);
+		t.start();
+	}
+	public void stopSending(){
+		isSending=false;
+		
+		ArrayList<Object> args = new ArrayList<Object>();
+        args.add("");
+        OSCMessage msg = new OSCMessage("/", args); // Dummymessage to quit run loop
+        try {
+			send(msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * Send an OSC packet (message or bundle) to the receiver we are bound to.
+	 * @param aPacket the bundle or message to send
+	 */
+	public void send(OSCPacket aPacket) throws IOException {
+		oscMessageQueue.add(aPacket);
+	}
+
+	@Override
+	public void run() {
+		oscMessageQueue = new ArrayBlockingQueue<OSCPacket>(25);
+		isSending = true;
+		OSCPacket aPacket;
+		while (isSending) {
+			try {
+				aPacket = oscMessageQueue.take();
+				
+				byte[] byteArray = aPacket.getByteArray();  /////Send OSC message//////
+				DatagramPacket packet = new DatagramPacket(byteArray, byteArray.length, address, getPort());
+				getSocket().send(packet);                   ///// End send       /////
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}		
+	}
+	public void stop(){
+		isSending = false;
+		oscMessageQueue.add(new OSCMessage("localhost")); //Empty message to get out of sendloop
+		this.close();
+	}
+	private InetAddress address;
+	private ArrayBlockingQueue<OSCPacket> oscMessageQueue;
+	private boolean isSending;
+}
---
+++ ReaperToolbarRemote/src/com/illposed/osc/OSCServer.java
@@ -0,0 +1,50 @@
+package com.illposed.osc;
+
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Date;
+
+public class OSCServer implements Runnable , OSCListener {
+	
+	
+	public OSCServer(InetAddress ipAddress, int outPort, int inPort) {
+		super();
+		this.ipAddress = ipAddress;
+		this.outPort = outPort;
+		this.inPort = inPort;
+		try {
+			oscPortOut = new OSCPortOut(ipAddress,outPort);
+			oscPortIn = new OSCPortIn(inPort);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public void addListener(String anAddress, OSCListener listener) {
+		oscPortIn.addListener(anAddress, listener);
+	}
+	@Override
+	public void run() {
+		oscPortOut.startSender();
+		oscPortIn.startListening();
+	}
+	public void stop(){
+		oscPortOut.stopSending();
+		oscPortIn.stopListening();
+	}
+	public void kill(){
+		oscPortOut.stopSending();
+		oscPortIn.stopListening();
+		oscPortIn.close();
+		oscPortOut.close();
+	}
+	private OSCPortOut oscPortOut;
+	private OSCPortIn oscPortIn;
+	private InetAddress ipAddress;
+	private int outPort;
+	private int inPort;
+	@Override
+	public void acceptMessage(Date time, OSCMessage message) {
+		
+	}
+}
